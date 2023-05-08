d4a048c07228655f10642a425509757cb739a7fb
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-05-08 21:27:31 +0700

    Chức năng đăng nhập

--- app/src/main/java/com/example/tablayoutfragviewpager/LoginActivity.java
+++ app/src/main/java/com/example/tablayoutfragviewpager/LoginActivity.java
@@ -1,20 +1,104 @@
 package com.example.tablayoutfragviewpager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tablayoutfragviewpager.databinding.ActivityLoginBinding;
+import com.example.tablayoutfragviewpager.models.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
-
+    ActivityLoginBinding binding;
+    FirebaseDatabase db;
+    DatabaseReference reference;
+    EditText etLoginEmail, etLoginPassword;
+    Button btnLogin;
     TextView txt_register;
+    String email, password, fullname;
+    Users user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        txt_register = (TextView) findViewById(R.id.tv_register);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        txt_register = binding.tvRegister;
+        etLoginEmail = binding.etLoginEmail;
+        etLoginPassword = binding.etLoginPassword;
+        btnLogin = binding.btnLogin;
+
+        // tham chiếu đến bảng users
+        db = FirebaseDatabase.getInstance();
+        reference = db.getReference("users");
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Kiểm tra tính hợp lệ của email
+                if (!validate(etLoginEmail.getText().toString())) {
+                    etLoginEmail.setError("Vui lòng nhập địa chỉ email hợp lệ!");
+                    return;
+                }
+                if (TextUtils.isEmpty(etLoginPassword.getText().toString())) {
+                    etLoginPassword.setError("Vui lòng nhập mật khẩu!");
+                    return;
+                }
+
+                // lấy dữ liệu lên
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // lấy thẻ trên database
+                        email = snapshot.child(etLoginEmail.getText().toString().replace(".", ",")).child("username").getValue(String.class);
+                        password = snapshot.child(etLoginEmail.getText().toString().replace(".", ",")).child("password").getValue(String.class);
+                        fullname = snapshot.child(etLoginEmail.getText().toString().replace(".", ",")).child("fullname").getValue(String.class);
+
+                        if (email != null && etLoginPassword.getText().toString().equals(password)) {
+                            user = new Users(email, password, fullname);
+                            SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            Gson gson = new Gson();
+                            String json = gson.toJson(user); // Chuyển đổi User thành chuỗi JSON
+                            editor.putString("user", json);
+                            editor.apply();
+                            // chuyển đến MainActivity
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công với tên " + user.getFullname(), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sai email hoặc mật khẩu", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(LoginActivity.this, "Sai email hoặc mật khẩu", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+
         txt_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +106,12 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean validate(String email) {
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
--- app/src/main/java/com/example/tablayoutfragviewpager/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/tablayoutfragviewpager/fragment/ProfileFragment.java
@@ -1,12 +1,18 @@
 package com.example.tablayoutfragviewpager.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,19 +21,29 @@ import androidx.fragment.app.Fragment;
 import com.example.tablayoutfragviewpager.LoginActivity;
 import com.example.tablayoutfragviewpager.models.News;
 import com.example.tablayoutfragviewpager.R;
+import com.example.tablayoutfragviewpager.models.Users;
+import com.google.gson.Gson;
+
+import static android.content.Context.MODE_PRIVATE;
 
 import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
     ArrayList<News> mDataList;
     ListView mListView;
-    Button btn_login;
+    Button btn_login, btn_logout;
+    TextView fullname;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+
         btn_login = view.findViewById(R.id.btn_login);
+        btn_logout = view.findViewById(R.id.btn_logout);
+        fullname = view.findViewById(R.id.fullname);
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,8 +51,43 @@ public class ProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        SharedPreferences preferences = getActivity().getSharedPreferences("MyPreferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        try {
+            // Lấy User từ SharedPreferences
+            String jvson = preferences.getString("user", "");
+            Users user = gson.fromJson(jvson, Users.class); // Chuyển đổi chuỗi JSON thành đối tượng User
+            if (user == null) {
+                // Nếu người dùng chưa đăng nhập, ẩn nút đăng xuất
+                btn_logout.setVisibility(View.GONE);
+            } else {
+                // Nếu người dùng đã đăng nhập, hiển thị nút đăng xuất và đăng nhập
+                btn_logout.setVisibility(View.VISIBLE);
+                btn_login.setVisibility(View.GONE);
+                fullname.setText(user.getFullname());
+            }
+//            Toast.makeText(getActivity(), user.getFullname(), Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+//            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("error" , e.getMessage());
+        }
 
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getActivity().getSharedPreferences("MyPreferences", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.remove("user");
+                editor.apply();
+                // Hiển thị thông báo cho người dùng
+                Toast.makeText(getActivity(), "Đăng xuất thành công", Toast.LENGTH_LONG).show();
+                btn_logout.setVisibility(View.GONE);
+                btn_login.setVisibility(View.VISIBLE);
+                fullname.setText("Trang cá nhân");
+            }
+        });
         return view;
+
     }
 
 }
