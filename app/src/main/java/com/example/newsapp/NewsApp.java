b53403050f32696f236d17300b813eb7696f656d
ntandat20130221 <20130221@st.hcmuaf.edu.vn>
2023-05-11 16:32:55 +0700

    Them chuc nang hien thi thong bao

---
+++ app/src/main/java/com/example/newsapp/NewsApp.java
@@ -0,0 +1,32 @@
+package com.example.newsapp;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+/**
+ * Lớp này sẽ tự chạy khi ứng dụng bắt đầu.
+ * Dùng để tạo NotificationChannel.
+ */
+public class NewsApp extends Application {
+    public static final String CHANNEL_ID = "news_channel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    /**
+     * Tạo NotificationChannel.
+     */
+    private void createNotificationChannel() {
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+}
