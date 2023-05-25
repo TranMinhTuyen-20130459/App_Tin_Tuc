db23659a9f81da04c1941fa3a816030711d87cd1
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2023-05-25 17:14:05 +0700

    chức năng quản lí danh mục

--- app/src/main/java/com/example/newsapp/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/newsapp/adapter/CategoryAdapter.java
@@ -1,12 +1,16 @@
 package com.example.newsapp.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,6 +43,8 @@ public class CategoryAdapter extends ArrayAdapter<Categories> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = convertView;
+        final String title = myListCategory.get(position).getTitle();
+        final String id = myListCategory.get(position).getPosition();
         if (view == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             view = inflater.inflate(R.layout.row_category, null);
@@ -62,26 +68,99 @@ public class CategoryAdapter extends ArrayAdapter<Categories> {
             @Override
             public void onClick(View v) {
                 // Thực hiện các tác vụ khi nút "Delete" được click trong item của ListView
-                System.out.println("Hello");
-                System.out.println(txtTitle);
-                System.out.println(txtSTT);
+                // Hiển thị hộp thoại xác nhận xóa
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("Bạn có chắc muốn xóa '" + title + "' không?");
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        myListCategory.remove(position);
+                        notifyDataSetChanged();
+
+                        Toast.makeText(getContext(), "Xóa thành công", Toast.LENGTH_SHORT).show();
+                        // xử lí firebase
+                    }
+                });
+                builder.setNegativeButton("Không", null);
+                builder.show();
+
             }
         });
 
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Thực hiện các tác vụ khi nút "Edit" được click trong item của ListView
-                System.out.println("Hellllllo");
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Edit Category");
+
+                // Inflate layout cho hộp thoại
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                View dialogView = inflater.inflate(R.layout.dialog_edit_category, null);
+                builder.setView(dialogView);
+
+                // Ánh xạ các thành phần trong hộp thoại
+                EditText etTitle = dialogView.findViewById(R.id.etTitle);
+                EditText etUrl = dialogView.findViewById(R.id.etUrl);
+                Spinner spinnerStatus = dialogView.findViewById(R.id.spinnerStatus);
+
+                // Lấy dữ liệu từ danh mục hiện tại
+                String currentTitle = myListCategory.get(position).getTitle();
+                String currentUrl = myListCategory.get(position).getUrl();
+                String currentStatus = myListCategory.get(position).getActive();
+
+                // Đặt giá trị ban đầu cho các thành phần trong hộp thoại
+                etTitle.setText(currentTitle);
+                etUrl.setText(currentUrl);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, new String[]{"0", "1"});
+                spinnerStatus.setAdapter(adapter);
+                spinnerStatus.setAdapter(adapter);
+//                spinnerStatus.setSelection(Integer.parseInt(currentStatus));
+                spinnerStatus.setSelection(currentStatus.equals("1") ? 1 : 0);
+                // Xử lý logic khi nhấn nút "Save" trong hộp thoại
+                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Lấy giá trị từ các thành phần trong hộp thoại sau khi người dùng thay đổi
+                        String editedTitle = etTitle.getText().toString().trim();
+                        String editedUrl = etUrl.getText().toString().trim();
+                        String editedStatus = String.valueOf(spinnerStatus.getSelectedItemPosition());
+
+                        // Cập nhật giá trị của danh mục trong danh sách và cập nhật giao diện
+                        myListCategory.get(position).setTitle(editedTitle);
+                        myListCategory.get(position).setUrl(editedUrl);
+                        myListCategory.get(position).setActive(editedStatus);
+                        notifyDataSetChanged();
+
+                        // cập nhập xuống firebase
+                        Toast.makeText(getContext(), "Category updated", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Xử lý logic khi nhấn nút "Cancel" trong hộp thoại
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Không thực hiện thay đổi khi nhấn nút "Cancel"
+                        dialog.dismiss();
+                    }
+                });
+
+                // Hiển thị hộp thoại
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
+
 
         return view;
     }
 
     private static class ViewHolder {
+        TextView txtSTT;
+        TextView txtTitle;
+        TextView txtActive;
+        ImageView deleteButton;
         ImageView editButton;
-        // Các view khác trong item
-        // ...
     }
 }
--- app/src/main/java/com/example/newsapp/fragment/ManageCategoryFragment.java
+++ app/src/main/java/com/example/newsapp/fragment/ManageCategoryFragment.java
@@ -1,11 +1,15 @@
 package com.example.newsapp.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -23,6 +27,8 @@ import com.example.newsapp.adapter.DataListAdapter;
 import com.example.newsapp.models.Categories;
 import com.example.newsapp.utils.Constants;
 
+import org.w3c.dom.Text;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,25 +36,91 @@ import java.util.List;
 public class ManageCategoryFragment extends Fragment {
     ListView mListView;
     ArrayList<Categories> listCategory;
+    public void init() {
+        listCategory = new ArrayList<>();
+        // Tạo danh sách đối tượng Person
+        listCategory.add(new Categories("1", "https://vnexpress.net/rss/tin-moi-nhat.rss", "tin mới", "1"));
+        listCategory.add(new Categories("2", "https://vnexpress.net/rss/tin-moi-nhat.rss", "thể thao", "1"));
+        listCategory.add(new Categories("3", "https://vnexpress.net/rss/tin-moi-nhat.rss", "thời sự", "1"));
+        listCategory.add(new Categories("4", "https://vnexpress.net/rss/tin-moi-nhat.rss", "giáo dục", "1"));
+    }
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.manage_category_fragment, container, false);
-        listCategory = new ArrayList<>();
+        init();
 
-
-        // Tạo danh sách đối tượng Person
-        listCategory.add(new Categories("1", "https://vnexpress.net/rss/tin-moi-nhat.rss", "tin mới", "1"));
-        listCategory.add(new Categories("2", "https://vnexpress.net/rss/tin-moi-nhat.rss", "tin mới", "1"));
-        listCategory.add(new Categories("3", "https://vnexpress.net/rss/tin-moi-nhat.rss", "tin mới", "1"));
-        listCategory.add(new Categories("4", "https://vnexpress.net/rss/tin-moi-nhat.rss", "tin mới", "1"));
-
+        // dùng adapter hiển thị lên listview category
         mListView = view.findViewById(R.id.list_view);
         CategoryAdapter adapter = new CategoryAdapter(getContext(), android.R.layout.simple_list_item_1, listCategory);
         mListView.setAdapter(adapter);
 
+        // button search, tìm kiếm
+        Button btnSearch = view.findViewById(R.id.btnSearchCate);
+        EditText etSearch = view.findViewById(R.id.search_name);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xử lí tìm kiếm
+                String name = etSearch.getText().toString().trim();
+                ArrayList<Categories> listCategorySearch = new ArrayList<>();
+
+                for(Categories c : listCategory) {
+                    if(c.getTitle().contains(name)) listCategorySearch.add(c);
+                }
+
+                // dùng adapter hiển thị lên listview category
+                mListView = view.findViewById(R.id.list_view);
+                CategoryAdapter adapter = new CategoryAdapter(getContext(), android.R.layout.simple_list_item_1, listCategorySearch);
+                mListView.setAdapter(adapter);
+            }
+        });
+
+
+        // form add category
+        Button btnAddCate = view.findViewById(R.id.btnAddCate);
+        btnAddCate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hiển thị hộp thoại form Dialog
+                showDialogForm();
+            }
+        });
+
         return view;
+
     }
+    private void showDialogForm() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_add_category, null);
+        builder.setView(dialogView);
+
+        EditText etUrl = dialogView.findViewById(R.id.editTextUrl);
+        EditText etTitle = dialogView.findViewById(R.id.editTextTitle);
+
+        builder.setPositiveButton("Thêm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String url = etUrl.getText().toString();
+                String title = etTitle.getText().toString();
+
+                // Thực hiện các tác vụ khi người dùng nhấn nút "Thêm"
+                Toast.makeText(getContext(), "Thêm danh mục: " + title, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
