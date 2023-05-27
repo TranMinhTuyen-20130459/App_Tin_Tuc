a3077b223a5577f0b9e984abdc0594f40b7268b1
CaoNhatNam <117261347+CaoNhatNam@users.noreply.github.com>
2023-05-27 14:05:08 +0700

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,30 @@
+package com.example.newsapp.utils;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String KEY_VIEWED_NEWS = "viewed_news";
+    public static final String LIST_TOTAL_CATE = "dataList";
+    public static final String KEY_LIST_NEWS_MAIN = "main"; // danh sách bài viết của danh mục chính ( trang chủ)
+    public static final String KEY_LIST_NEWS_AUXI = "data"; // danh sách bài viết của danh mục phụ
+    public static final String KEY_NEWS_DETAILS = "link"; // key lúc gửi qua chi tiết bài viết
+    public static long QUANTITY_NEWS_OF_CATE = 20; // số bài viết trong 1 danh mục
+    public static String ROLE_ADMIN = "admin"; // quyền admin
+    public static String ROLE_CUSTOMER = "user"; // quyền user
+    public static String TABLE_USERS = "users"; // tên bảng users
+    public static String TABLE_CATEGORIES = "categories"; // tên bảng danh mục
+
+    public static  int FONT_SIZE = 20;
+
+    public static void setFontSize(int fontSize) {
+        FONT_SIZE = fontSize;
+    }
+
+    // sửa số lượng bài viết trên trong danh mục
+    public static void setQuantityNewsOfCate(int quantityNewsOfCate) {
+        QUANTITY_NEWS_OF_CATE = quantityNewsOfCate;
+    }
+
+}
---
+++ WidgetFragment.java
@@ -0,0 +1,130 @@
+package com.example.newsapp.fragment;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.newsapp.R;
+import com.example.newsapp.utils.Constants;
+
+import androidx.core.content.ContextCompat;
+public class WidgetFragment extends Fragment {
+    private TextView textView;
+    TextView font_size, quantity, test;
+
+    TextView title;
+    int fontsize = 0;
+
+    int font;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.widget_fragment, container, false);
+        View view1 = inflater.inflate(R.layout.news_item, container, true);
+        title = view1.findViewById(R.id.title);
+        font_size = view.findViewById(R.id.font_size);
+        quantity = view.findViewById(R.id.quantity);
+        quantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Chỉnh số bài viết");
+
+                // Tạo một EditText để nhập số
+                final EditText inputEditText = new EditText(getContext());
+                inputEditText.setInputType(InputType.TYPE_CLASS_NUMBER); // Chỉ cho phép nhập số
+                inputEditText.setHint("Số bài viết"); // Gợi ý trong ô nhập
+
+                // Tạo một icon số bài viết
+                Drawable icon = ContextCompat.getDrawable(getContext(), R.drawable.btn_home);
+                builder.setIcon(icon);
+                builder.setView(inputEditText);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputText = inputEditText.getText().toString();
+                        if (!TextUtils.isEmpty(inputText)) {
+                            int articleCount = Integer.parseInt(inputText);
+                            if (articleCount > 0 && articleCount < 60) {
+                                // Sử dụng giá trị articleCount tùy ý ở đây
+                                Constants.setQuantityNewsOfCate(articleCount);
+                            } else {
+                                // Hiển thị thông báo lỗi nếu số không thỏa mãn điều kiện
+                                Toast.makeText(getContext(), "Số bài viết không hợp lệ!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("Hủy", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        font_size.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Chọn cỡ chữ");
+
+                final String[] fontSizeOptions = {"Nhỏ", "Bình thường", "Lớn"};
+                int checkedItem = 1; // Vị trí của "Bình thường" trong mảng fontSizeOptions
+
+                builder.setSingleChoiceItems(fontSizeOptions, checkedItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0: // Nhỏ
+                                font = 15;
+                                break;
+                            case 1: // Bình thường
+                                font = 20;
+                                break;
+                            case 2: // Lớn
+                                font = 22;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Constants.setFontSize(font);
+                    }
+                });
+
+                builder.setNegativeButton("Hủy", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
+        });
+        return view;
+
+    }
+}
